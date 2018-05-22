d172c041aa8e2dce76f54cbaf894533a2905df16
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2018-05-22 11:59:10 +0200

    Updated and ready to be revised by Lara

--- www/circ/retour_xarxa.inc.php
+++ www/circ/retour_xarxa.inc.php
@@ -33,17 +33,19 @@ $link2 = @mysql_connect(SQL_SERVER, USER_NAME, USER_PASS) OR die("Error MySQL");
 $q ='SELECT expl_cb FROM exemplaires';
 $q = 'select exemplaires.expl_cb , notices.tit1 from pret, exemplaires,empr, notices where notices.notice_id=exemplaires.expl_notice and pret.pret_idempr=empr.id_empr and pret.pret_idexpl=exemplaires.expl_id and empr.id_empr='.$form_id_empr.' order by pret.pret_date desc';
 
-echo "<table><tr><th>ID</th><th>Titulo</th><th>Estado</th><th>Cambiar estado</th><th>Retirar</th><th>Devuelve</th></tr>";
+$q = 'select exemplaires.expl_cb , notices.tit1, docs_statut.statut_libelle from pret, exemplaires,empr, notices,docs_statut where notices.notice_id=exemplaires.expl_notice and pret.pret_idempr=empr.id_empr and pret.pret_idexpl=exemplaires.expl_id and exemplaires.expl_statut=docs_statut.idstatut and empr.id_empr='.$form_id_empr.' order by pret.pret_date desc ';
+
+echo "<table><tr><th>ID</th><th>Titulo</th><th>Estado</th><th>Devolver</th><th>Retirar</th><th>No Devolver</th></tr>";
 
 	$resultData = @mysql_query($q, $link2);
 		if (@mysql_num_rows($resultData) != 0) {
 			while ($rowData = mysql_fetch_array($resultData)) {	
 				echo "<tr>";
-				echo "<td> <a href=http://biblioteca.ieslasenia.org/circ.php?categ=retour&cb_expl=".$rowData[0].">".$rowData[1]."</a></td><td>".$rowData[1]."</td>";
-				echo "<td> Molando </td>";
-				echo "<td> <input type=checkbox name=usado".$rowData[0]." value=usado checked> Usado </td>";
+				echo "<td> <a href=http://biblioteca.ieslasenia.org/circ.php?categ=retour&cb_expl=".$rowData[0].">".$rowData[0]."</a></td><td>".$rowData[1]."</td>";
+				echo "<td> ".$rowData[2]." </td>";
+				echo "<td> <input type=radio name=devolucion".$rowData[0]." value=devolver checked> Devolver</td>";
 				echo "<td> <input type=radio name=devolucion".$rowData[0]." value=retirar> Retirar </td>";
-				echo "<td> <input type=radio name=devolucion".$rowData[0]." value=devolver checked> Devolver </td>";
+				echo "<td> <input type=radio name=devolucion".$rowData[0]." value=nodevolver> No Devolver </td>";
 				echo "</tr>";
 
 
